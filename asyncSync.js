f589a5755f1500da1f760781a6eb8f6523878e96
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-04-04 19:10:26 +0000

    syncAsync

---
+++ asyncSync.js
@@ -0,0 +1,18 @@
+// setTimeout //
+function execute() {
+  console.log('1');
+    setTimeout(() => {
+      console.log('2');
+    }, 3000);
+  console.log('3');
+}
+
+execute(); // 1, 3, 2 
+
+
+print();
+function print() {
+  console.log('Hello');
+}
+let hi = 'hi';
+console.log(hi);
